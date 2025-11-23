d516d6f61cea2f80b56067d490e6b2c34b2edd32
André Pereira Martins <andre.pereira.martins100@gmail.com>
2025-11-23 20:05:45 -0300

    update build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "5ded0894b4cdca5958f5a80b321cc0c6",
+const RESOURCES = {"flutter_bootstrap.js": "71eddc55be0e8ad5b3a2777235d0f9f8",
 "version.json": "4f789f0bd76c26a04273a7d4310d75d2",
 "index.html": "6013ff1f479e1f9633b160e534b6fd25",
 "/": "6013ff1f479e1f9633b160e534b6fd25",
 "apple-touch-icon.png": "42ceee23f037f677831208b6197c5d77",
-"main.dart.js": "460f7d5528bea287acf7d17a151c91e5",
+"main.dart.js": "c59e9683083625cb4e4589d635391cf2",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "7b94627b96779c727f520d7ec1033aa5",
 "icons/Icon-192.png": "9c3e3070b27d3c705b52d4e5151acdeb",
