82c31e55063f38602796d58fca21cc4bfe29e2d1
André Pereira Martins <andre.pereira.martins100@gmail.com>
2026-07-22 20:32:46 -0300

    Update goal_providers.dart

--- lib/features/goals/presentation/providers/goal_providers.dart
+++ lib/features/goals/presentation/providers/goal_providers.dart
@@ -4,9 +4,11 @@ import 'package:film_fount/features/goals/data/datasources/goal_datasource.dart'
 import 'package:film_fount/features/goals/data/repositories/goal_repository_impl.dart';
 import 'package:film_fount/features/goals/domain/entities/goal_entity.dart';
 import 'package:film_fount/features/goals/domain/repositories/goal_repository.dart';
+import 'package:film_fount/features/goals/domain/usecases/add_goal_usecase.dart';
 import 'package:film_fount/features/goals/domain/usecases/get_goals_usecase.dart';
 import 'package:film_fount/features/goals/presentation/notifiers/goal_notifier.dart';
 import 'package:film_fount/features/goals/presentation/notifiers/goal_register_notifier.dart';
+import 'package:film_fount/features/library/presentation/providers/library_providers.dart';
 import 'package:film_fount/features/search/presentation/providers/search_providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -25,9 +27,17 @@ final getGoalsUseCaseProvider = Provider<GetGoalsUsecase>(
   (ref) => GetGoalsUsecase(ref.watch(goalRepositoryProvider)),
 );
 
+final addGoalUseCaseProvider = Provider<AddGoalUseCase>(
+  (ref) => AddGoalUseCase(ref.watch(goalRepositoryProvider)),
+);
+
 final goalNotifierProvider =
-    StateNotifierProvider<GoalNotifier, AppState<GoalListEntity>>(
-      (ref) => GoalNotifier(ref.watch(getGoalsUseCaseProvider)),
+    StateNotifierProvider.autoDispose<GoalNotifier, AppState<GoalListEntity>>(
+      (ref) => GoalNotifier(
+        ref.watch(getGoalsUseCaseProvider),
+        ref.watch(addGoalUseCaseProvider),
+        ref.watch(libraryDatasourceProvider),
+      ),
     );
 
 final goalRegisterNotifierProvider =
